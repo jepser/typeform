86cbe0ef88cb33180fcd19a2208f5b3a1d5f2b57
Alex Bass <alexpjbass@gmail.com>
2017-02-08 18:49:15 +0100

    [PS-318] UI dialogue with buttons

--- src/js/Builder.jsx
+++ src/js/Builder.jsx
@@ -2,24 +2,54 @@
 
 import * as React from 'react'
 
-const buttonAttributes = {
-  className: 'button-primary',
-  style: {
-    marginRight: '.5em',
-  },
-}
+const { pluginRoot, userEmail } = typeformObject
 
-function Field (props) {
-
+function Field ({ label, name, placeholder, inputType = 'text' }) {
   return (
-    <label htmlFor="">
-      Name:
-      <input className="regular-text" type="text" name="name" placeholder="Hi there! What's your name?"/>
+    <label className="tf-form-group" htmlFor={`input_${name}`}>
+      <span className="tf-form-group__label">{label}:</span>
+      <input className="tf-form-group__input regular-text"
+             type={inputType}
+             name={name}
+             id={`input_${name}`}
+             placeholder={placeholder} />
     </label>
   )
 }
 
-export default class TypeformEmbed extends React.Component {
+class EmbedButton extends React.Component {
+  constructor (props) {
+    super(props)
+    this.setActive = this.setActive.bind(this)
+  }
+
+  setActive (ev) {
+    const fill = this.props.isActive ? '#2c82bd' : '#aaa'
+    const icon = ev.currentTarget.contentDocument
+    icon.querySelector('#form-bg').style.fill = fill
+  }
+
+  componentDidUpdate () {
+    this.setActive({ currentTarget: this.icon })
+  }
+
+  render () {
+    const { icon, children, isActive, onClick } = this.props
+    return (
+      <div className={`embed-option ${isActive ? 'is-active' : ''}`}>
+        <button className="embed-option__button" type="button" onClick={onClick}>
+          <div className="embed-option__cover"></div>
+          <object data={`${pluginRoot}/assets/images/${icon}`}
+                  onLoad={this.setActive}
+                  ref={icon => this.icon = icon} />
+        </button>
+        <div className="embed-option__label" onClick={onClick}>{children}</div>
+      </div>
+    )
+  }
+}
+
+export default class ShortcodeBuilder extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -40,28 +70,43 @@ export default class TypeformEmbed extends React.Component {
     }
   }
 
+  setEmbedType (embedType) {
+    this.setState({ embedType })
+  }
+
   render () {
     const style = tab => ({ style: this.getStyle(tab) })
-    const onClick = tab => ({ onClick: () => this.setTab(tab) })
+    const setTab = tab => ({ onClick: () => this.setTab(tab) })
+    const setEmbedType = type => () => this.setEmbedType(type)
+
+    const { embedType } = this.state
+
+    const tabAttributes = {
+      className: 'button-primary',
+      style: {
+        marginRight: '.5em',
+      },
+    }
 
     return (
       <div className="typeform-embed-widget tf-embed" style={{ width: '400px', height: '300px' }}>
-        <button {...buttonAttributes} {...onClick('create')}>Start from scratch</button>
-        <button {...buttonAttributes} {...onClick('embed')}>Embed an existing Typeform</button>
+        <button {...tabAttributes} {...setTab('create')}>Start from scratch</button>
+        <button {...tabAttributes} {...setTab('embed')}>Embed an existing Typeform</button>
         <div className="tf-embed__tabs">
           <form className="tf-embed__tab tf-embed__tab--create" {...style('create')}>
-            <label htmlFor="">
-              Name:
-              <input className="regular-text" type="text" name="name" placeholder="Hi there! What's your name?"/>
-            </label>
-            <label htmlFor="">
-              Email:
-              <input className="regular-text" type="email" name="name" placeholder="Great! What's your email?" value={typeformObject.userEmail}/>
-            </label>
-            <label htmlFor="">
-              Message:
-              <input className="regular-text" type="text" name="name" placeholder="Thanks! What would you like to say?"/>
-            </label>
+            <fieldset>
+              <Field label="Name" name="name" placeholder="Hey there! What's your name?" />
+              <Field label="Email" name="email" placeholder="Great! And your email?" inputType="email" />
+              <Field label="Message" name="message" placeholder="Thanks! How can we help?" />
+            </fieldset>
+            <fieldset>
+              <legend>Embedding options</legend>
+              <div className="embedding-options">
+                <EmbedButton icon="embed.svg" isActive={embedType === 'embed'} onClick={setEmbedType('embed')}>Embed</EmbedButton>
+                <EmbedButton icon="popup.svg" isActive={embedType === 'popup'} onClick={setEmbedType('popup')}>Popup</EmbedButton>
+                <EmbedButton icon="drawer.svg" isActive={embedType === 'drawer'} onClick={setEmbedType('drawer')}>Drawer</EmbedButton>
+              </div>
+            </fieldset>
           </form>
           <form className="tf-embed__tab tf-embed__tab--url" {...style('embed')}>
             Add yo URL
--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,80 @@
-/* global wp, jQuery, tinymce, tinyMCE */
+/* global wp, jQuery, tinymce, tinyMCE, editor */
 
 'use strict'
 
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import TypeformEmbed from './Builder.jsx'
+import ShortcodeBuilder from './Builder.jsx'
 
-console.log(typeformObject)
+const { pluginRoot, userEmail } = typeformObject
 
-
+const WINDOW_TEMPLATE = '<div class="typeform-embed-app loading"></div>'
 const SHORTCODE_TAG = 'typeform_embed'
-const WINDOW_TEMPLATE = '<div class="typeform-embed-app loading" style="width:400px;height:300px"></div>'
+const SHORTCODE_FIELDS = [
+  'url',
+  'type',
+  'height',
+  'width',
+  'style',
+  'button_text',
+  'pass_params',
+]
+
+const windowConfiguration = {
+  title: 'Add a Contact Form',
+  body: [{
+    type: 'container',
+    html: WINDOW_TEMPLATE,
+  }],
+
+  onopen (ev) {
+    return setTimeout(() => {
+      const wrapper = ev.target.$el.context.querySelector('.typeform-embed-app')
+      ReactDOM.render(<ShortcodeBuilder />, wrapper, () => wrapper.classList.remove('loading'))
+    }, 100)
+  },
+
+  onclose (ev) {
+    const elem = ev.target.$el.context.querySelector('.mce-last .mce-container-body .mce-container-body')
+    ReactDOM.unmountComponentAtNode(elem.firstElementChild)
+  },
+
+  onsubmit (ev) {
+    const args = {
+      tag: SHORTCODE_TAG,
+      type: 'single',
+      content: ev.data.innercontent,
+      attrs: {},
+    }
+    console.log(editor, 'editor')
+    args.attrs = buildShortcodeStructure(ev.data)
+    editor.insertContent(wp.shortcode.string(args))
+  },
+}
 
 jQuery(function ($) {
   const media = wp.media
   const template = media.template('editor-tf-banner')
 
-  const shortcodeFields = [
-    'url',
-    'type',
-    'height',
-    'width',
-    'style',
-    'button_text',
-    'pass_params',
-  ]
-
   wp.mce = wp.mce || {}
   wp.mce.typeformRender = {
     shortcode_data: {},
     template: template,
+
+    View: {
+      template: template,
+      postID: $('#post_ID').val(),
+
+      initialize (options) {
+        this.shortcode = options.shortcode
+        typeformRender.shortcode_data = this.shortcode
+      },
+
+      getHtml () {
+        const options = this.shortcode.attrs.named
+        return this.template(options)
+      },
+    },
 
     getContent () {
       const options = this.shortcode.attrs.named
@@ -37,31 +82,18 @@ jQuery(function ($) {
       return this.template(options)
     },
 
-    View: {
-      template: template,
-      postID: $('#post_ID').val(),
-      initialize (options) {
-        this.shortcode = options.shortcode
-        typeformRender.shortcode_data = this.shortcode
-      },
-      getHtml () {
-        const options = this.shortcode.attrs.named
-        return this.template(options)
-      },
-    },
-
-    edit (node) {
+    edit (/* node */) {
       const attrs = this.shortcode.attrs.named
       this.popupwindow(tinyMCE.activeEditor, attrs)
     },
 
     popupwindow (editor, values, onsubmit) {
       setTimeout(() => {
-        const enable = (values.type === 'embed' || values.type === '')
+        const enable = values.type === 'embed' || values.type === ''
         return setEmbedFormFields(enable)
       }, 10)
 
-      openMediaWindow(values, editor)
+      openMediaWindow(editor, values)
     },
   }
 
@@ -69,48 +101,8 @@ jQuery(function ($) {
 
   $('#add-typeform').click(openMediaWindow)
 
-  function openMediaWindow (values, editor) {
+  function openMediaWindow (editor = tinymce.activeEditor, values = {}) {
     console.log(editor, 'editor initialize')
-    if (!values) {
-      values = {}
-    }
-
-    if (!editor) {
-      editor = tinymce.activeEditor
-    }
-
-    const windowConfiguration = {
-      title: 'Add a typeform',
-      body: [{
-        type: 'container',
-        html: WINDOW_TEMPLATE,
-      }],
-
-      onopen (ev) {
-        return setTimeout(() => {
-          const wrapper = ev.target.$el.context.querySelector('.typeform-embed-app')
-          ReactDOM.render(<TypeformEmbed />, wrapper, () => wrapper.classList.remove('loading'))
-        }, 100)
-      },
-
-      onclose (ev) {
-        const elem = ev.target.$el.context.querySelector('.mce-last .mce-container-body .mce-container-body')
-        ReactDOM.unmountComponentAtNode(elem.firstElementChild)
-      },
-
-      onsubmit (ev) {
-        const args = {
-          tag: SHORTCODE_TAG,
-          type: 'single',
-          content: ev.data.innercontent,
-          attrs: {},
-        }
-        console.log(editor, 'editor')
-        args.attrs = buildShortcodeStructure(ev.data)
-        editor.insertContent(wp.shortcode.string(args))
-      },
-    }
-
     if (tinymce.activeEditor) {
       tinymce.activeEditor.windowManager.open(windowConfiguration)
     } else {
@@ -120,10 +112,11 @@ jQuery(function ($) {
 
   function buildShortcodeStructure (attrs) {
     const fields = {}
-    const prefix = 'tf_'
-    $.each(shortcodeFields, function (i, field) {
-      if (attrs[prefix + field]) {
-        fields[field] = attrs[prefix + field]
+
+    SHORTCODE_FIELDS.forEach(attr => {
+      const key = `tf_${attr}`
+      if (attrs[key]) {
+        fields[attr] = attrs[key]
       }
     })
 
