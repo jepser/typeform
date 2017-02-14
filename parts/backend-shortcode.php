8c997d8134c594789a8c1d206dadfa8c8d724a1a
Alex Bass <alexpjbass@gmail.com>
2017-02-14 18:54:27 +0100

    Add shortcode template for “builder”

--- parts/backend-shortcode.php
+++ parts/backend-shortcode.php
@@ -2,7 +2,7 @@
     <div class="tf-embed-wrapper" id="tf_{{ data.id }}">
         <div class="tf-content">
             <# if ( data.url ) { #>
-                <a href="{{ data.url }}" class="link dtbaker_button_light">Edit embed settings</a>
+                <div class="edit-link">Edit embed settings</div>
                 <div class="tf-embed-wrapper__info">
                     <span class="tf-embed-wrapper__label">Url:</span>
                     <span class="tf-embed-wrapper__value">{{ data.url }}</span>
@@ -11,36 +11,47 @@
                     <span class="tf-embed-wrapper__label">Type:</span>
                     <span class="tf-embed-wrapper__value">{{ data.type }}</span>
                 </div>
+            <# } else if ( data.builder ) { #>
+            <div class="edit-link">Edit form settings</div>
+            <div class="tf-embed-wrapper__info">
+                <span class="tf-embed-wrapper__label">Type:</span>
+                <span class="tf-embed-wrapper__value">{{ data.type }}</span>
+            </div>
             <# } else { #>
                 <span>No URL provided.</span>
             <# } #>
         </div>
     </div>
     <style>
-    .tf-embed-wrapper{
-        background-color: #BAE0E6;
+    .tf-embed-wrapper {
+        background-color: #bae0e6;
         padding: 10px;
         overflow: hidden;
-        color: #72A985;
+        color: #72a985;
         text-align: center;
         height: 60px;
-        border: 1px solid #73BEC8;
+        border: 1px solid #73bec8;
+        border-radius: 4px;
     }
-    .tf-embed-wrapper a{
+    .tf-embed-wrapper .edit-link {
+        display: block;
+        color: #333;
+    }
+    .tf-embed-wrapper a {
         border: 0;
         display: block;
         text-decoration: none;
         text-align: left;
     }
-    .tf-embed-wrapper::before{
+    .tf-embed-wrapper::before {
         width: 26px;
         height: 30px;
         content: '';
         display: inline-block;
         margin-right: 10px;
         background: url(<?php echo tf_plugin_url() . 'assets/images/attention.png' ?>) no-repeat left center/26px auto;
-    }
-    .tf-embed-wrapper .tf-content{
+    } 
+    .tf-embed-wrapper .tf-content {
         display: inline-block;
         vertical-align: top;
         line-height: 30px;
