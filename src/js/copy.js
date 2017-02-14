d3daa7d4fd080d5d2c26ea5d2f30e7587f432fa8
Alex Bass <alexpjbass@gmail.com>
2017-02-14 16:26:19 +0100

    Store text in a separate script (ease of editing I guess)

---
+++ src/js/copy.js
@@ -0,0 +1,23 @@
+'use strict'
+
+export default {
+  windowTitle: `Add a Contact Form`,
+
+  createTab: `Start from scratch`,
+  embedTab: `Embed an existing Typeform`,
+  nameField: `Hey there! What's your name?`,
+  emailField: `Great! And your email?`,
+  messageField: `Thanks! How can we help?`,
+  responsesField: `your.email@example.com`,
+  urlField: `http://example.typeform.com/to/Z6Agtz`,
+
+  embedOptions: `Embedding options`,
+  embedButton: `Embed`,
+  popupButton: `Popup`,
+  drawerButton: `Drawer`,
+
+  linkLabel: `Link`,
+  buttonLabel: `Button`,
+
+  linkText: `Launch me!`,
+}
