1f19b6523e17977507b6b5d0afafc6ec1e45f77b
Alex Bass <alexpjbass@gmail.com>
2017-02-14 16:30:59 +0100

    Shortcode inserter: works in text mode & removed redundant stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,14 @@
-/* global wp, jQuery, tinymce, tinyMCE, editor */
+/* global wp, jQuery, tinyMCE, editor, wpActiveEditor */
 
 'use strict'
 
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import ShortcodeBuilder from './Builder.jsx'
-
-const { pluginRoot, userEmail } = typeformObject
+import Builder from './Builder.jsx'
 
 const WINDOW_TEMPLATE = '<div class="typeform-embed-app loading"></div>'
 const SHORTCODE_TAG = 'typeform_embed'
+
 const SHORTCODE_FIELDS = [
   'url',
   'type',
@@ -17,11 +16,33 @@ const SHORTCODE_FIELDS = [
   'width',
   'style',
   'button_text',
-  'pass_params',
+  'builder',
 ]
 
-const windowConfiguration = {
+const getTextEditor = () => {
+  return document.getElementById(wpActiveEditor)
+}
+
+const cursorPosition = input => {
+  return [
+    input.selectionStart,
+    input.selectionEnd,
+  ]
+}
+
+const insertAtCursor = (input, content) => {
+  const { value } = input
+  const [ start, end ] = cursorPosition(input)
+  input.value = [
+    value.substr(0, start),
+    content,
+    value.substr(end, value.length),
+  ].join('')
+}
+
+const windowConfiguration = (editor, values = {}) => ({
   title: 'Add a Contact Form',
+
   body: [{
     type: 'container',
     html: WINDOW_TEMPLATE,
@@ -30,67 +51,42 @@ const windowConfiguration = {
   onopen (ev) {
     return setTimeout(() => {
       const wrapper = ev.target.$el.context.querySelector('.typeform-embed-app')
-      ReactDOM.render(<ShortcodeBuilder />, wrapper, () => wrapper.classList.remove('loading'))
+      ReactDOM.render(<Builder />, wrapper, () => wrapper.classList.remove('loading'))
     }, 100)
   },
 
   onclose (ev) {
-    const elem = ev.target.$el.context.querySelector('.mce-last .mce-container-body .mce-container-body')
+    const elem = ev.target.$el.context.querySelector('.typeform-embed-app')
     ReactDOM.unmountComponentAtNode(elem.firstElementChild)
   },
 
   onsubmit (ev) {
-    const args = {
+    const json = ev.target.$el.context.querySelector('.typeform-embed-values').innerHTML
+
+    const shortcodeContent = wp.shortcode.string({
       tag: SHORTCODE_TAG,
       type: 'single',
       content: ev.data.innercontent,
-      attrs: {},
+      attrs: JSON.parse(json),
+    })
+
+    console.log(shortcodeContent)
+
+    if (editor && !editor.hidden) {
+      return editor.insertContent(shortcodeContent)
+    } else {
+      const textEditor = getTextEditor()
+      return insertAtCursor(textEditor, shortcodeContent)
     }
-    console.log(editor, 'editor')
-    args.attrs = buildShortcodeStructure(ev.data)
-    editor.insertContent(wp.shortcode.string(args))
   },
-}
+})
 
-function openMediaWindow (editor = tinymce.activeEditor, values = {}) {
-  console.log(editor, 'editor initialize')
-  if (tinymce.activeEditor) {
-    tinymce.activeEditor.windowManager.open(windowConfiguration)
+function openMediaWindow (values = {}, editor = tinyMCE.activeEditor) {
+  if (tinyMCE.activeEditor) {
+    tinyMCE.activeEditor.windowManager.open(windowConfiguration(editor, values))
   } else {
-    tinymce.windowManager.open(windowConfiguration)
+    tinyMCE.windowManager.open(windowConfiguration(editor, values))
   }
-}
-
-function buildShortcodeStructure (attrs) {
-  const fields = {}
-
-  SHORTCODE_FIELDS.forEach(attr => {
-    const key = `tf_${attr}`
-    if (attrs[key]) {
-      fields[attr] = attrs[key]
-    }
-  })
-
-  return fields
-}
-
-function setEmbedFormFields (isEmbed) {
-  const $embedFields = $('#tf_width, #tf_height')
-  const $linkFields = $('#tf_style, #tf_button_text')
-
-  if (isEmbed) {
-    enableFormContainer($embedFields, true)
-    enableFormContainer($linkFields, false)
-  } else {
-    enableFormContainer($embedFields, false)
-    enableFormContainer($linkFields, true)
-  }
-}
-
-function enableFormContainer ($elem, enable) {
-  $elem.prop('disabled', !enable).closest('.mce-container-body').css({
-    opacity: (enable) ? 1 : 0.2,
-  })
 }
 
 jQuery(function ($) {
@@ -129,16 +125,11 @@ jQuery(function ($) {
     },
 
     popupwindow (editor, values, onsubmit) {
-      setTimeout(() => {
-        const enable = values.type === 'embed' || values.type === ''
-        return setEmbedFormFields(enable)
-      }, 10)
-
-      openMediaWindow(editor, values)
+      openMediaWindow(values, editor)
     },
   }
 
   wp.mce.views.register(SHORTCODE_TAG, wp.mce.typeformRender)
 
-  $('#add-typeform').click(openMediaWindow)
+  $('#add-typeform').on('click', openMediaWindow)
 })
