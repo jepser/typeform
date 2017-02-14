3b901fe4a6d662686d3f09744d0893d96aeed498
Alex Bass <alexpjbass@gmail.com>
2017-02-14 16:25:40 +0100

    Move editor shortcode to main stylesheet, and make it sassy

--- parts/backend-shortcode.php
+++ parts/backend-shortcode.php
@@ -16,45 +16,4 @@
             <# } #>
         </div>
     </div>
-    <style>
-    .tf-embed-wrapper{
-        background-color: #BAE0E6;
-        padding: 10px;
-        overflow: hidden;
-        color: #72A985;
-        text-align: center;
-        height: 60px;
-        border: 1px solid #73BEC8;
-    }
-    .tf-embed-wrapper a{
-        border: 0;
-        display: block;
-        text-decoration: none;
-        text-align: left;
-    }
-    .tf-embed-wrapper::before{
-        width: 26px;
-        height: 30px;
-        content: '';
-        display: inline-block;
-        margin-right: 10px;
-        background: url(<?php echo tf_plugin_url() . 'assets/images/attention.png' ?>) no-repeat left center/26px auto;
-    }
-    .tf-embed-wrapper .tf-content{
-        display: inline-block;
-        vertical-align: top;
-        line-height: 30px;
-    }
-    .tf-embed-wrapper__info {
-        display: inline-block;
-        font-size: 11px;
-        color: #444;
-    }
-    .tf-embed-wrapper__label {
-        font-weight: bold;
-    }
-    .tf-embed-wrapper__value {
-        
-    }
-    </style>
 </div>
