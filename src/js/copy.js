0bcc04397a521fc435042252808260557e6bcc2f
Alex Bass <alexpjbass@gmail.com>
2017-02-16 12:56:02 +0100

    Add new copy

--- src/js/copy.js
+++ src/js/copy.js
@@ -3,13 +3,24 @@
 export default {
   windowTitle: `Add a Contact Form`,
 
-  createTab: `Start from scratch`,
-  embedTab: `Embed an existing Typeform`,
+  createDescription: `Need a simple contact form? Here’s one we made earlier…`,
+  addDescription: `Already have a Typeform account? Well this shouldn't take long…`,
+
+  createTab: `Build a basic typeform`,
+  embedTab: `Embed an existing typeform`,
+  nameLabel: `Name field`,
+  emailLabel: `Email field`,
+  messageLabel: `Message field`,
   nameField: `Hey there! What's your name?`,
   emailField: `Great! And your email?`,
   messageField: `Thanks! How can we help?`,
-  responsesField: `your.email@example.com`,
   urlField: `http://example.typeform.com/to/Z6Agtz`,
+
+  responsesFieldset: `Responses`,
+  responsesDesc: `You’ll receive your responses by email, but since you haven’t signed up to typeform yet, ` +
+    `you won’t be able to see the data later on. If that works for you, you’re good to go! Just tell us where ` +
+    `to send the responses in the field below. `,
+  responsesField: `your.email@example.com`,
 
   embedOptions: `Embedding options`,
   embedButton: `Embed`,
